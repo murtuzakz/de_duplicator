1ed04b408da21dfd63f206dab202182b149466b2
Murtuza <murtuzafirst@gmail.com>
2013-10-24 19:43:31 +0530

    Initial commit. My first Gem! yaygit status

---
+++ de_duplicator.gemspec
@@ -0,0 +1,13 @@
+Gem::Specification.new do |s|
+  s.name        = 'de_duplicator'
+  s.version     = '0.0.0'
+  s.date        = '2013-10-24'
+  s.summary     = "DeDuplicate stuff!"
+  s.description = "A gem to remove duplicates"
+  s.authors     = ["Murtuza Kutub"]
+  s.email       = 'murtuzafirst@gmail.com'
+  s.files       = ["lib/de_duplicator.rb"]
+  s.homepage    =
+    'http://rubygems.org/gems/de_duplicator'
+  s.license       = 'MIT'
+end
---
+++ lib/de_duplicator.rb
@@ -0,0 +1,31 @@
+class DeDuplicator
+	
+	attr_accessor :base_list, :new_additions
+
+	def initialize( base_list=[] )
+		@base_list = base_list.flatten.uniq
+    @new_additions = []
+	end
+
+	def update_base_list(list)
+    flat_list = list.flatten
+    @new_additions = flat_list - (@base_list & flat_list)
+		@base_list = (@base_list + @new_additions).flatten.uniq
+	end
+
+	def unique_list
+		base_list
+	end
+
+	def get_duplicates(list)
+    flat_list = list.flatten
+		duplicates = (@base_list & flat_list)
+		update_base_list(flat_list)
+		duplicates
+  end
+
+  def remove_recent_additions!
+    @base_list -= @new_additions
+  end
+
+end
